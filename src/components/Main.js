0ae31238c38d7f4ed9a52198b7b8ce38f0251438
Kimberly Bell <kimberly.bell@tma1.com>
2018-07-13 19:37:03 -0400

    filter section wip

--- src/components/Main.js
+++ src/components/Main.js
@@ -7,15 +7,38 @@ import Gear     from 'IMG/gear-service-pro.png';
 import Users    from 'IMG/users-commercial-pro.png'; 
 import json     from './../data/dealers.json';
 
+const resultType = [
+  {name: 'service'},
+  {name: 'installation'},
+  {name: 'residential'},
+  {name: 'commercial'}
+];
+
+const capitalize = (word) => {
+  return (word.charAt(0).toUpperCase() + word.slice(1));
+};
+
 const Main = () => {
   const {dealers} = json;
+
   return(
     <main>
       <div className="container">
         <div className="filter-container">
-          <p>filter here</p>
+          <p className="d-inline">{dealers.length} dealers in {json.zipcode}</p>
+          <span> | </span>
+          <p className="d-inline"><b>Filter Results</b></p>
+          {resultType.map(result => {
+            return(
+              <div className="d-inline">
+                <input type="checkbox" id={result.name} name="results" value={result.name} checked />
+                <label htmlFor={result.name}>{capitalize(result.name)}</label>
+              </div>
+            );
+          })}
         </div>
         {dealers.map(dealer => {
+          const hours = dealer.data.weekHours;
           return(
             <div className="card">
               <div className="card-header">
@@ -26,22 +49,30 @@ const Main = () => {
                   <img src={Phone} alt="Phone icon" />
                   <p>{dealer.data.phone1}</p>
                 </div>
-                <p><i>Can't talk now? Click below to send an e-mail.</i></p>
+                <p><i>Can\'t talk now? Click below to send an e-mail.</i></p>
                 <button className="btn-card">
                   Contact this Pro
                 </button>
-                <p><b>Business Hourse</b></p>
+                <p><b>Business Hours</b></p>
                 <ul>
-                  <li>List of hours</li>
-                  <li>more hrs</li>
-                  <li>more hrs</li>
+                  <li>Weekdays {hours.mon}</li>
+                  <li>Saturdays {!hours.sat ? hours.mon : hours.sat}</li>
+                  <li>Sundays {!hours.sun ? 'CLOSED' : hours.sun}</li>
                 </ul>
               </div>
               <div className="card-footer">
-                <img src={Star} alt="Star icon" />
-                <img src={Home} alt="House icon" />
-                <img src={Gear} alt="Gear icon" />
-                <img src={Users} alt="Users icon" />
+                <div className="service-certification">
+                  <img src={Star} alt="Star icon" />
+                </div>
+                <div className="service-certification">
+                  <img src={Gear} alt="Gear icon" />
+                </div>
+                <div className="service-certification">
+                  <img src={Home} alt="House icon" />
+                </div>
+                <div className="service-certification">
+                  <img src={Users} alt="Users icon" />
+                </div>
               </div>
             </div>
           );
